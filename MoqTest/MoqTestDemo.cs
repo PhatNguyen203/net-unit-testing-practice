3848731d2b6a98047ebe1b61c7299e7e546e4f8a
Lee0ne203 <Lee0ne203@gmail.com>
2021-08-26 23:52:47 -0400

    added mock test cases

--- MoqTest/MoqTestDemo.cs
+++ MoqTest/MoqTestDemo.cs
@@ -1,6 +1,10 @@
+using Autofac.Extras.Moq;
 using DemoLibrary.Logic;
 using DemoLibrary.Model;
+using DemoLibrary.Utilities;
+using Moq;
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace MoqTest
@@ -39,7 +43,67 @@ namespace MoqTest
 			Assert.Equal(expectedResut.FirstName, actualResult.FirstName);
 			Assert.Equal(expectedResut.LastName, actualResult.LastName);
 			Assert.Equal(expectedResut.HeightInInches, actualResult.HeightInInches);
+		}
+		[Fact]
+		public void LoadPeople_ValidCall()
+		{
+			using (var mock = AutoMock.GetLoose())
+			{
+				mock.Mock<ISqlServerDataAccess>()
+					.Setup(m => m.LoadData<PersonModel>("Select * from Person"))
+					.Returns(GetSamplePeople);
+				
+				var cls = mock.Create<PersonProcessor>();
+				var actualResult = cls.LoadPeople();
+				var expectedResult = GetSamplePeople();
 
+				Assert.True(actualResult != null);
+				Assert.Equal(expectedResult.Count, actualResult.Count);
+				for (int i = 0; i < expectedResult.Count; i++)
+				{
+					Assert.Equal(expectedResult[i].FirstName, actualResult[i].FirstName);
+					Assert.Equal(expectedResult[i].LastName, actualResult[i].LastName);
+				}
+			}
+		}
+		[Fact]
+		public void SavePeople_ValidCall()
+		{
+			using(var mock = AutoMock.GetLoose())
+			{
+				var person = new PersonModel()
+				{
+					FirstName = "test",
+					LastName = "test",
+					HeightInInches = 77
+				};
+				var query = "insert into Person (FirstName, LastName, HeightInInches) " +
+				"values (@FirstName, @LastName, @HeightInInches)";
+
+				mock.Mock<ISqlServerDataAccess>()
+					.Setup(m => m.SaveData(person,query));
+				var cls = mock.Create<PersonProcessor>();
+				cls.SavePerson(person); // Save is void
+				mock.Mock<ISqlServerDataAccess>()
+					.Verify(m => m.SaveData(person,query), Times.Exactly(1));
+			}
+		}
+			private List<PersonModel> GetSamplePeople()
+		{
+			List<PersonModel> output = new List<PersonModel>
+			{
+				new PersonModel
+				{
+					FirstName = "John",
+					LastName = "Doe"
+				},
+				new PersonModel
+				{
+					FirstName = "Tim",
+					LastName = "Corey"
+				}
+			};
+			return output;
 		}
 	}
 }
