531214b6dd9018782b73802946df9521d92e683b
Lee0ne203 <Lee0ne203@gmail.com>
2021-08-26 23:21:10 -0400

    Added second test case

--- MoqTest/MoqTestDemo.cs
+++ MoqTest/MoqTestDemo.cs
@@ -1,4 +1,5 @@
 using DemoLibrary.Logic;
+using DemoLibrary.Model;
 using System;
 using Xunit;
 
@@ -20,6 +21,25 @@ namespace MoqTest
 
 			Assert.Equal(expectedIsValid, actual.isValid);
 			Assert.Equal(expectedHeightInInches, actual.heightInInches);
+		}
+		[Theory]
+		[InlineData("Tim", "Corey", "6'8\"", 80)]
+		[InlineData("Charitry", "Corey", "5'4\"", 64)]
+		public void CreatePerson_Successful(string firstName, string lastName, string heightText, double expectedHeight)
+		{
+			PersonProcessor person = new PersonProcessor(null);
+			PersonModel expectedResut = new PersonModel()
+			{
+				FirstName = firstName,
+				LastName = lastName,
+				HeightInInches = expectedHeight
+			};
+			var actualResult = person.CreatePerson(firstName, lastName, heightText);
+
+			Assert.Equal(expectedResut.FirstName, actualResult.FirstName);
+			Assert.Equal(expectedResut.LastName, actualResult.LastName);
+			Assert.Equal(expectedResut.HeightInInches, actualResult.HeightInInches);
+
 		}
 	}
 }
